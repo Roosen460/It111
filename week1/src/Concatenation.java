68332e572293a4f449b970ed3dafcd6032d2e4e6
Roosen460 <bayous.charoll1@gmail.com>
2025-04-22 11:00:18 -0700

    Add files via upload

---
+++ week1/src/Concatenation.java
@@ -0,0 +1,24 @@
+public class Concatenation
+{
+
+    public static void main(String[] args)
+    {
+
+        String color = "red";
+        int money = 2_000_000;
+
+        System.out.println("My favorite color is " +color);
+        System.out.println("I hit the jackpot and won $" +money+ " dollars!");
+        System.out.println(color+" "+money);
+
+        System.out.print("This will be my statement regarding my favorite color is "+color+" and will");
+        System.out.print(" let you know that i won $ " +money+  " dollars");
+
+
+
+
+    }
+
+
+
+}
